b70eb0242be814379a628a3607e9d18cb587e332
Benjamin Baird <benjaminrbaird@gmail.com>
2021-10-02 02:07:39 -0400

    TicTacToeModel first implentation

--- src/main/java/edu/wofford/TicTacToeModel.java
+++ src/main/java/edu/wofford/TicTacToeModel.java
@@ -1,6 +1,128 @@
 package edu.wofford;
 
+import javax.swing.border.Border;
+
+import org.assertj.swing.dependency.fest_util.ToString;
+
+import java.lang.Math.*;
 
 public class TicTacToeModel {
+    // 2d array with values of -1, 0, or 1. "O" -> -1, blank -> 0, "X" -> 1
+    private int[][] board;
+    
+    //-1 -> player "O"'s turn, 1 -> player "X"'s turn
+    private int turn;
 
+
+    public TicTacToeModel() {
+        board = new int[3][3];
+        for (int i=0; i<3; i++) {
+            for (int j=0; j<3; j++) {
+                board[i][j] = 0;
+            }
+        }
+        turn = 1;
+        
+    }
+
+
+    public String getMark(int x, int y) {
+        if (x < 0 || x > 2 || y < 0 || y > 2) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+
+        if (board[x][y] == 1) {
+            return "X";
+        } else if (board[x][y] == -1) {
+            return "O";
+        }
+        return " ";
+    }
+
+
+    private boolean isValidAtLocation(int x, int y) {
+        if (x < 0 || x > 2 || y < 0 || y > 2) {
+            return false;
+        }
+        return board[x][y] == 0;
+    }
+
+
+    public boolean markBoard(int x, int y) {
+        if (isValidAtLocation(x, y)) {
+            board[x][y] = turn;
+            turn *= -1;
+            return true;
+        }
+        return false;
+    }
+
+
+    private int sumRow(int row) {
+        int sum = 0;
+        for(int i=0; i<3; i++) {
+            sum += board[row][i];
+        }
+        return sum;
+    }
+
+
+    private int sumCol(int col) {
+        int sum = 0;
+        for(int i=0; i<3; i++) {
+            sum += board[i][col];
+        }
+        return sum;
+    }
+
+
+    private int sumTopLeftToBottomRightDiagonal() {
+        return board[0][0] + board[1][1] + board[2][2];
+    }
+
+
+    private int sumTopRightToBottomLeftDiagonal() {
+        return board[0][2] + board[1][1] + board[2][0];
+    }
+
+
+    public boolean isOver() {
+        boolean three_in_a_row = false;
+
+        for (int i=0; i<3; i++) {
+            three_in_a_row = three_in_a_row || (Math.abs(sumCol(i)) == 3);
+            three_in_a_row = three_in_a_row || (Math.abs(sumRow(i)) == 3);          
+        }
+        three_in_a_row = three_in_a_row || (Math.abs(sumTopLeftToBottomRightDiagonal()) == 3);
+        three_in_a_row = three_in_a_row || (Math.abs(sumTopRightToBottomLeftDiagonal()) == 3);
+
+        return three_in_a_row;
+    }
+
+
+    public String getPlayerTurn() {
+        if (turn == 1) {
+            return "X";
+        }
+        return "O";
+    }
+
+
+    public String toString() {
+        String board_string = "";
+
+        for (int i=0; i<3; i++) {
+            for (int j=0; j<3; j++) {
+                board_string += getMark(i, j);
+                if (j < 2) {
+                    board_string += "|";
+                }
+            }
+
+            if (i < 2) {
+                board_string += "\n-----\n";
+            }
+        }
+        return board_string;
+    }
 }
--- src/test/java/edu/wofford/TicTacToeModelTest.java
+++ src/test/java/edu/wofford/TicTacToeModelTest.java
@@ -5,7 +5,60 @@ import static org.junit.Assert.*;
 
 public class TicTacToeModelTest {
    @Test
-   public void testSomething() {
-       assertTrue(true);
+   public void testNewBoard() {
+       TicTacToeModel testBoard = new TicTacToeModel();
+       boolean allBlank = true;
+       for (int i=0; i<3; i++) {
+           for (int j=0; j<3; j++) {
+                allBlank = allBlank && (testBoard.getMark(i, j) == " ");
+           }
+       }
+       assertTrue(allBlank);
+   }
+
+
+   @Test
+   public void testFirstTurn() {
+        TicTacToeModel testBoard = new TicTacToeModel();
+        assertEquals("X", testBoard.getPlayerTurn());
+   }
+
+
+   @Test
+   public void testMark1() {
+        TicTacToeModel testBoard = new TicTacToeModel();
+        testBoard.markBoard(1, 1);
+        assertEquals("X", testBoard.getMark(1, 1));
+   }
+
+
+   @Test
+   public void testMark2() {
+        TicTacToeModel testBoard = new TicTacToeModel();
+        testBoard.markBoard(1, 1);
+        testBoard.markBoard(1, 1);
+        assertEquals("X", testBoard.getMark(1, 1));
+   }
+
+
+   @Test
+   public void testMark3() {
+        TicTacToeModel testBoard = new TicTacToeModel();
+        testBoard.markBoard(1, 1);
+        testBoard.markBoard(1, 1);
+        testBoard.markBoard(1, 2);
+        assertEquals("O", testBoard.getMark(1, 2));
+   }
+
+
+   @Test
+   public void testIsOver() {
+        TicTacToeModel testBoard = new TicTacToeModel();
+        testBoard.markBoard(0, 0);
+        testBoard.markBoard(1, 1);
+        testBoard.markBoard(0, 1);
+        testBoard.markBoard(1, 2);
+        testBoard.markBoard(0, 2);
+        assertTrue(testBoard.isOver());
    }
 }
